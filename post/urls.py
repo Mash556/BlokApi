a2637b8f421f6348aea618e4fe4cda2e6362d347
Mash556 <almashnurmatk.gmail.com>
2023-12-25 15:31:57 +0600

    post

--- post/urls.py
+++ post/urls.py
@@ -2,12 +2,27 @@ from django.urls import path, include
 from .views import *
 from rest_framework.routers import DefaultRouter
 
-# Создаем экземпляр DefaultRouter
-router = DefaultRouter()
+# # Создаем экземпляр DefaultRouter
+# router = DefaultRouter()
 
-# Регистрируем YourModelViewSet в router
-router.register(r'posts', PostModelViewSet, basename='post')
+# # Регистрируем YourModelViewSet в router
+# router.register(r'posts', PostModelViewSet, basename='post')
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
+
+"""====================filter========================="""
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+# router = DefaultRouter()
+# router.register(r'posts', PostViewSet, basename='post')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('', PostListCreateAPIView.as_view())
 ]
--- post/views.py
+++ post/views.py
@@ -3,6 +3,9 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
 from .models import Post
 
+import django_filters
+from post.filters import PostModelFilter
+
 # class PostListCreateAPIView(generics.ListCreateAPIView):
 #     queryset = Post.objects.all()
 #     serializer_class = PostSerializer
@@ -33,3 +36,26 @@ class PostModelViewSet(ModelViewSet):
 
 # .filter(title='asdf').all()
 # mixins | generics
+# написать логику поиска и филтрации для постов
+        
+
+class PostListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_class = PostModelFilter
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.query_params.get('search', None)
+        if search_query:
+            queryset = queryset.filter(title__icontains=search_query)
+        return queryset
+
+
+
+
+
